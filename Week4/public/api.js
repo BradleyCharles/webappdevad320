b4070252aacfd9f5831db7559141713841681990
BradleyCharles <bradgcharles@gmail.com>
2024-01-29 18:34:51 -0800

    Finished dog api

--- Week4/public/api.js
+++ Week4/public/api.js
@@ -1,11 +1,10 @@
-export async function fetchDogBreeds() {
+async function fetchDogBreeds() {
   try {
     const response = await fetch('https://dogapi.dog/api/v2/breeds');
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-
     const breedInfo = data.data.map(breed => {
       return {
         name: breed.attributes.name,
@@ -32,3 +31,46 @@ export async function fetchDogBreeds() {
     return [];
   }
 }
+
+async function updateName() {
+  try {
+    const dogNames = await fetchDogBreeds();
+    
+    for (let i = 0; i < 10; i++) {
+      const breed = dogNames[i].name;
+      const dogNameElement = document.getElementById(`dogName${i}`);
+      dogNameElement.innerHTML = breed;
+    }
+
+  } catch (error) {
+    console.error('Error updating dog name:', error.message);
+  }
+} updateName();
+
+export async function updateDog(i = 0) {
+  try {
+    const dogBreeds = await fetchDogBreeds();
+    const dogName = dogBreeds[i].name;
+      const dogLife = dogBreeds[i].lifeSpan.min + ' - ' + dogBreeds[i].lifeSpan.max;
+      const dogWeightM = dogBreeds[i].maleWeight.min + ' - ' + dogBreeds[i].maleWeight.max;
+      const dogWeightF = dogBreeds[i].femaleWeight.min + ' - ' + dogBreeds[i].femaleWeight.max;
+      const dogAlergy = dogBreeds[i].hypoallergenic;
+      const dogDesc = dogBreeds[i].description;
+
+    const dogNameElement = document.getElementById("dogName");
+    dogNameElement.innerHTML = dogName;
+    const dogLifeElement = document.getElementById("dogLife");
+    dogLifeElement.innerHTML = dogLife;
+    const dogWeightMElement = document.getElementById("dogWeightM");
+    dogWeightMElement.innerHTML = dogWeightM;
+    const dogWeightFElement = document.getElementById("dogWeightF");
+    dogWeightFElement.innerHTML = dogWeightF;
+    const dogAlergyElement = document.getElementById("dogAlergy");
+    dogAlergyElement.innerHTML = dogAlergy;
+    const dogDescElement = document.getElementById("dogDesc");
+    dogDescElement.innerHTML = dogDesc;
+
+  } catch (error) {
+    console.error('Error updating dog facts:', error.message);
+  }
+} updateDog(Math.floor(Math.random()*10));
