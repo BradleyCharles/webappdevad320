f072b11dc684c51e4e5658db99ed13a0344d2d27
BradleyCharles <bradgcharles@gmail.com>
2024-02-16 15:08:52 -0800

    new

--- webappdevad320/feature-toggle-demo/src/App.js
+++ webappdevad320/feature-toggle-demo/src/App.js
@@ -1,17 +1,24 @@
-import FeatureToggle from './FeatureToggle.js'
-import './App.css';
+import FeatureToggle from "./FeatureToggle.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Features!</p>
-    <FeatureToggle isEnabled={true} featureName='Feature #1' />
-    <FeatureToggle isEnabled={true} featureName='Feature #2' />
-    <FeatureToggle isEnabled={false} featureName='Feature #3' />
-    <FeatureToggle isEnabled={true} featureName='Feature #4' />
-    <FeatureToggle isEnabled={true} featureName='Feature #5' />
+      <div className="Features">
+        <p>Features!</p>
+        <FeatureToggle isEnabled={true} featureName="Feature #1" />
+        <FeatureToggle isEnabled={true} featureName="Feature #2" />
+        <FeatureToggle isEnabled={false} featureName="Feature #3" />
+        <FeatureToggle isEnabled={true} featureName="Feature #4" />
+        <FeatureToggle isEnabled={true} featureName="Feature #5" />
+      </div>
     </div>
   );
 }
+var new = variable;
 
 export default App;
+
+
+
+var new = variable;
--- webappdevad320/feature-toggle-demo/src/FeatureToggle.js
+++ webappdevad320/feature-toggle-demo/src/FeatureToggle.js
@@ -1,8 +1,8 @@
 export default function FeatureToggle({isEnabled, featureName}) {
-        if (isEnabled) {
+        if (isEnabled) { //If statement to decide if the name of the feature whould be rendered
             return <div>{featureName}</div>;
             
         }
-        return '<Missing Feature>';
+        return '<Missing Feature>'; //Return "Missing" if the feature is not enabled. Can also be Null
 }
 
