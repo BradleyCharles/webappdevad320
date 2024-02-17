10ccd813ec1013e0156b3627f970242787bab243
BradleyCharles <bradgcharles@gmail.com>
2024-02-16 19:07:39 -0800

    ProductList

---
+++ webappdevad320/static-product/src/App.js
@@ -0,0 +1,13 @@
+import ProductList from './components/ProductList.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Catalog</h1>
+    <ProductList  />
+    </div>
+  );
+}
+
+export default App;
