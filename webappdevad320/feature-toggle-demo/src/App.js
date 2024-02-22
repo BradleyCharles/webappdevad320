ba7c51df7cd1ef6ed06220c0830f787ecf7bac5e
BradleyCharles <bradgcharles@gmail.com>
2024-02-22 12:06:28 -0800

    added back feature toggle

---
+++ webappdevad320/feature-toggle-demo/src/App.js
@@ -0,0 +1,20 @@
+import FeatureToggle from "./FeatureToggle.js";
+import "./App.css";
+
+function App() {
+  return ( //Returns the imported component FeatureToggle with the values of isEnabled and featureName
+    <div className="App">
+      <div className="Features">  
+        <p>Features!</p>
+        <FeatureToggle isEnabled={true} featureName="Feature #1" />
+        <FeatureToggle isEnabled={true} featureName="Feature #2" />
+        <FeatureToggle isEnabled={false} featureName="Feature #3" />
+        <FeatureToggle isEnabled={true} featureName="Feature #4" />
+        <FeatureToggle isEnabled={true} featureName="Feature #5" />
+      </div>
+    </div> 
+  );
+}
+
+
+export default App;
