ffac4f7ac3eaac6ef1d320d18d1d5ec19ab0ccee
BradleyCharles <bradgcharles@gmail.com>
2024-02-22 15:14:28 -0800

    new

--- webappdevad320/buttons/src/component/toolbar.js
+++ webappdevad320/buttons/src/component/toolbar.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-const CustomToolbar = ({message, children}) => {
-
+const CustomToolbar = ({ message, children }) => {
   const beep = () => {
-  
     alert(message);
   };
 
-  return (
-      <button onClick={beep}>
-        {children}
-      </button>
-  );
+  return <button onClick={beep}>{children}</button>;
 };
 
 export { CustomToolbar };
