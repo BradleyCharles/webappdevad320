d0bb4569cc8f941aacdd240e2c86f3fae4ca15b4
BradleyCharles <bradgcharles@gmail.com>
2024-02-22 10:33:31 -0800

    changes

---
+++ webappdevad320/buttons/src/FeatureToggle.js
@@ -0,0 +1,8 @@
+export default function FeatureToggle({isEnabled, featureName}) {
+        if (isEnabled) { //If statement to decide if the name of the feature should be rendered
+            return <div>{featureName}</div>;
+            
+        }
+        return '<Missing Feature>'; //Return "Missing" if the feature is not enabled. Can also be Null
+}
+
---
+++ webappdevad320/buttons/src/component/toolbar2.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CustomToolbar2 = ({ m, c }) => {
+  const array = [
+    { message: "Downloading! 1", children: "Download File 1" },
+    { message: "Downloading! 2", children: "Download File 2" },
+    { message: "Downloading! 3", children: "Download File 3" },
+    {},
+  ];
+
+  const arrayMessage = array[m].message;
+  const arrayChildren = array[c].children;
+
+  const beep = () => {
+    alert(arrayMessage);
+  };
+
+  return <button onClick={beep}>{arrayChildren}</button>;
+};
+
+export { CustomToolbar2 };
