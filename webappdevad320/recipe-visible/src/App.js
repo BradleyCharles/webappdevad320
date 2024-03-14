2ff93654a2181cdd2c24b0346f8b0f604f97fd4a
BradleyCharles <bradgcharles@gmail.com>
2024-03-14 09:43:15 -0700

    new

---
+++ webappdevad320/recipe-visible/src/App.js
@@ -0,0 +1,13 @@
+import RecipeGallery from "./components/RecipeGallery.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Recipes!</p>
+      <RecipeGallery />
+    </div>
+  );
+}
+
+export default App;
